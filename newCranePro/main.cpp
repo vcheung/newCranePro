b002fa83389a40407c5c6a036eca1e00e30a2944
vcheung <66822415@qq.com>
2013-08-21 16:11:15 +0800

    显示中文

--- newCranePro/main.cpp
+++ newCranePro/main.cpp
@@ -1,8 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("GBK"));
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("GBK"));
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("GBK"));
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
